3bffbff05f0286e90ecff9c635dfb8e20734996a
Heang Ly <heanglypa@gmail.com>
2021-04-09 07:49:09 -0400

    config post file

--- simple-blog/posts/index.js
+++ simple-blog/posts/index.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
-const axios = require('axios');
+const express = require('express')
+const { randomBytes } = require('crypto')
+const cors = require('cors')
+const axios = require('axios')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const posts = {};
+const posts = {}
 
 app.get('/posts', (req, res) => {
-  res.send(posts);
-});
+  res.send(posts)
+})
 
 app.post('/posts', async (req, res) => {
-  const id = randomBytes(4).toString('hex');
-  const { title } = req.body;
-  posts[id] = { id, title };
+  const id = randomBytes(4).toString('hex')
+  const { title } = req.body
+  posts[id] = { id, title }
 
   await axios.post('http://localhost:4005/events', {
     type: 'PostCreated',
     data: {
       id,
-      title,
-    },
-  });
+      title
+    }
+  })
 
-  res.status(201).send(posts[id]);
-});
+  res.status(201).send(posts[id])
+})
 
 app.post('/events', (req, res) => {
-  console.log('Event Received:', req.body.type);
-  res.send({});
-});
+  console.log('Event Received:', req.body.type)
+  res.send({})
+})
 
-app.listen(4000, () => console.log('==>POST<== PORT-4000'));
+app.listen(4000, () => {
+  console.log('v55')
+  console.log('==>POST<== PORT-4000')
+})
